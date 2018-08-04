a3a6df448084a78d02ab05fecccaea4937b43c9a
Brightcells <brightcells@gmail.com>
2018-08-05 00:59:19 +0800

    :art: Fetch image ext from res' Content-Type

--- pywe_wxa_qrcode/pywxaqrcode.py
+++ pywe_wxa_qrcode/pywxaqrcode.py
@@ -17,7 +17,7 @@ class WxaQRCode(BaseToken):
         self.CREATE_WXA_QRCODE = self.API_DOMAIN + '/cgi-bin/wxaapp/createwxaqrcode?access_token={access_token}'
 
     def get_wxa_code(self, path, width=430, auto_color=False, line_color={'r': '0', 'g': '0', 'b': '0'}, is_hyaline=False, appid=None, secret=None, token=None, storage=None, res_to_base64=True, data_uri_scheme=True):
-        data = self.post(
+        res = self.post(
             self.GET_WXA_CODE.format(access_token=final_access_token(self, appid=appid, secret=secret, token=token, storage=storage)),
             data={
                 'path': path,
@@ -29,11 +29,11 @@ class WxaQRCode(BaseToken):
             res_to_json=False
         )
         if not res_to_base64:
-            return data
-        return '{0}{1}'.format('data:image/png;base64,' if data_uri_scheme else '', base64.b64encode(data.content))
+            return res
+        return '{0}{1}'.format('data:image/{0};base64,'.format(res.headers.get('Content-Type', '').split('/')[-1] or 'jpeg') if data_uri_scheme else '', base64.b64encode(res.content))
 
     def get_wxa_code_unlimit(self, scene, page=None, width=430, auto_color=False, line_color={'r': '0', 'g': '0', 'b': '0'}, is_hyaline=False, appid=None, secret=None, token=None, storage=None, res_to_base64=True, data_uri_scheme=True):
-        data = self.post(
+        res = self.post(
             self.GET_WXA_CODE_UNLIMIT.format(access_token=final_access_token(self, appid=appid, secret=secret, token=token, storage=storage)),
             data={
                 'scene': scene,
@@ -46,11 +46,11 @@ class WxaQRCode(BaseToken):
             res_to_json=False
         )
         if not res_to_base64:
-            return data
-        return '{0}{1}'.format('data:image/png;base64,' if data_uri_scheme else '', base64.b64encode(data.content))
+            return res
+        return '{0}{1}'.format('data:image/{0};base64,'.format(res.headers.get('Content-Type', '').split('/')[-1] or 'jpeg') if data_uri_scheme else '', base64.b64encode(res.content))
 
     def create_wxa_qrcode(self, path, width=430, appid=None, secret=None, token=None, storage=None, res_to_base64=True, data_uri_scheme=True):
-        data = self.post(
+        res = self.post(
             self.CREATE_WXA_QRCODE.format(access_token=final_access_token(self, appid=appid, secret=secret, token=token, storage=storage)),
             data={
                 'path': path,
@@ -59,8 +59,8 @@ class WxaQRCode(BaseToken):
             res_to_json=False
         )
         if not res_to_base64:
-            return data
-        return '{0}{1}'.format('data:image/png;base64,' if data_uri_scheme else '', base64.b64encode(data.content))
+            return res
+        return '{0}{1}'.format('data:image/{0};base64,'.format(res.headers.get('Content-Type', '').split('/')[-1] or 'jpeg') if data_uri_scheme else '', base64.b64encode(res.content))
 
 
 wxaqrcode = WxaQRCode()
